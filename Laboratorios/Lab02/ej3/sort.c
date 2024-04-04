c6abda095ba7c496da17ad27bf1feb6756d4d170
Joaquin, Pereyra Muchiutti <joaquinpereyramuchiutti@gmail.com>
2024-04-04 18:25:57 -0300

    Add files via upload

---
+++ Laboratorios/Lab02/ej3/sort.c
@@ -0,0 +1,54 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "array_helpers.h"
+#include "sort_helpers.h"
+#include "sort.h"
+
+static unsigned int partition(int a[], unsigned int izq, unsigned int der)
+{
+    unsigned int pivot, i, j;
+    pivot = izq;
+    i = izq + 1u;
+    j = der;
+
+    while (i <= j)
+    {
+        if (goes_before(a[i], a[pivot]))
+        {
+            i = i + 1u;
+        }
+        else if (goes_before(a[pivot], a[j]))
+        {
+            j = j - 1u;
+        }
+        else if (goes_before(a[pivot], a[i]) && goes_before(a[j], a[pivot]))
+        {
+            swap(a, i, j);
+            i = i + 1u;
+            j = j - 1u;
+        }
+    }
+    swap(a, pivot, j);
+    pivot = j; // Nueva posición del pivot.
+
+    return pivot;
+}
+
+static void quick_sort_rec(int a[], unsigned int izq, unsigned int der)
+{
+    unsigned int pivot;
+    if (izq < der)
+    {
+        pivot = partition(a, izq, der);
+        quick_sort_rec(a, izq, (pivot == 0u) ? 0u : pivot - 1u);
+        quick_sort_rec(a, pivot + 1u, der);
+    }
+}
+
+void quick_sort(int a[], unsigned int length)
+{
+    quick_sort_rec(a, 0, (length == 0) ? 0 : length - 1);
+}
